c59ec139a6386e463df5ce062bdc9188ee8d1859
forsaken <alexey2142@mail.ru>
2017-02-26 12:06:34 +1000

    update import

--- lib/uphill_rating/import.ex
+++ lib/uphill_rating/import.ex
@@ -5,37 +5,79 @@ defmodule Import do
   alias UphillRating.Team
   alias UphillRating.Repo
 
-  def process("BicyclistRace", row) do
-    %BicyclistRace{
-      time: time(row),
-      bicyclist_id: bicyclist(row).id,
-      race_id: race(row).id,
-      team_id: team(row).id
-    }
+  def process_file(resource, file_path) do
+    status = :ok
+    messages = []
+    data = File.stream!(file_path)
+    |> CSV.decode(separator: ?;)
+    |> Enum.with_index
+    |> Enum.map(fn {row, i} ->
+      {st, obj, message} = process(resource, row)
+      if st != :ok do
+        status = :error
+        messages = [messages | "Error on line #{i + 1}: #{message}"]
+      end
+      obj
+    end)
+    {status, data, messages}
   end
 
-  def process("Bicyclist", row) do
-    %Bicyclist{
-      name: Enum.at(row, 0),
-      year: Enum.at(row, 1),
-      sex: Enum.at(row, 2),
-      team_id: team(row).id
-    }
+  defp process("BicyclistRace", row) do
+    time = time(row |> Enum.at 0)
+    bicyclist_name = row |> Enum.at 1
+    race_name = row |> Enum.at 2
+    team_name = row |> Enum.at 3
+
+    unless bicyclist = bicyclist(bicyclist_name) do
+      {:error, nil, "bicyclist \"#{bicyclist_name}\" not exist"}
+    else
+      unless race = race(race_name) do
+        {:error, nil, "race \"#{race_name}\" not exist"}
+      else
+        unless team = team(team_name) do
+          {:error, nil, "team \"#{team_name}\" not exist"}
+        else
+          obj = %BicyclistRace{
+            time: time,
+            bicyclist_id: bicyclist.id,
+            race_id: race.id,
+            team_id: team.id
+          }
+          {:ok, obj, nil}
+        end
+      end
+    end
   end
 
-  defp time(row) do
-    Enum.at(row, 0) |> String.split([":", "."]) |> Enum.with_index |> Enum.map(fn {e, i} -> if i == 3 do String.to_integer(e) * 100000 else String.to_integer e end end) |> List.to_tuple |> Ecto.Time.cast |> elem(1)
+  defp process("Bicyclist", row) do
+    team_name = Enum.at(row, 3)
+
+    unless team = team(team_name) do
+      {:error, nil, "team \"#{team_name}\" not exist"}
+    else
+      obj = %Bicyclist{
+        name: Enum.at(row, 0),
+        year: Enum.at(row, 1),
+        sex: Enum.at(row, 2),
+        team_id: team.id
+      }
+      {:ok, obj, nil}
+    end
   end
 
-  defp bicyclist(row) do
-    Bicyclist |> Repo.get_by! name: Enum.at(row, 1)
+  defp time(raw_time) do
+    raw_time |> String.split([":", "."]) |> Enum.with_index |> Enum.map(fn {e, i} -> if i == 3 do String.to_integer(e) * 100000 else String.to_integer e end end) |> List.to_tuple |> Ecto.Time.cast |> elem(1)
   end
 
-  defp race(row) do
-    Race |> Repo.get_by! name: Enum.at(row, 2)
+  defp bicyclist(name) do
+    Bicyclist |> Repo.get_by name: name
   end
 
-  defp team(row) do
-    Team |> Repo.get_by! name: Enum.at(row, 3)
+  defp race(name) do
+    Race |> Repo.get_by name: name
+  end
+
+  defp team(name) do
+    Team |> Repo.get_by name: name
   end
 end
--- web/controllers/admin_controller.ex
+++ web/controllers/admin_controller.ex
@@ -4,17 +4,24 @@ defmodule UphillRating.AdminController do
   alias UphillRating.Repo
 
   def import(conn, params) do
-    flash_message = "Import complete"
-    file_path = params["import"]["file"].path
-    resource = params["import"]["resource"]
+    flash_message = "Unknown error" 
+    
+    if params["import"]["file"] do
+      file_path = params["import"]["file"].path
+      resource = params["import"]["resource"]
 
-    if File.regular?(file_path) do
-      File.stream!(file_path)
-      |> CSV.decode(separator: ?;)
-      |> Enum.map(fn row -> Repo.insert! Import.process(resource, row) end)
+      if File.regular?(file_path) do
+        {status, prepared_data, messages} = Import.process_file(resource, file_path)
+        if status == :ok do
+          prepared_data |> Enum.map(fn e -> Repo.insert e end)
+        end
+        flash_message = messages |> Enum.join("\n")
+      end
+    else
+      flash_message = "File not selected"
     end
 
-    Phoenix.Controller.put_flash(conn, :notice, flash_message)
+    Phoenix.Controller.put_flash(conn, :notice, flash_message |> Phoenix.HTML.Format.text_to_html |> Phoenix.HTML.safe_to_string)
     |> Phoenix.Controller.redirect(to: ExAdmin.Utils.admin_resource_path(UphillRating.BicyclistRace, :index))
   end
 end
