35be5d07d3bb004a7f9ac5e7f7cf200bb3cd4f16
forsaken <alexey2142@mail.ru>
2017-06-20 15:43:58 +1000

    fix travis config

--- config/travis.exs
+++ config/travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :phoenix_trello, PhoenixTrello.Endpoint,
+config :uphill_rating, UphillRating.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,10 +10,10 @@ config :phoenix_trello, PhoenixTrello.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :phoenix_trello, PhoenixTrello.Repo,
+config :uphill_rating, UphillRating.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
-  database: "phoenix_trello_test",
+  database: "uphill_rating_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
